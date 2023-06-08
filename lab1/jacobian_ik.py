354aed0bafb05ae18cc10e7de9df19fdccf26903
qiufeng <qiufeng@sensetime.com>
2023-06-08 23:26:10 +0800

    implmemnt both left and right perturbation in jacobian computation, via so3 derivative

--- lab1/jacobian_ik.py
+++ lab1/jacobian_ik.py
@@ -40,7 +40,7 @@ Consider the target j as x_j, the function item of F(θ)_j, and the correspondin
              ... + 
              R_0 * R_1 * ... * R_{i-1} * R_i * R_{i+1} * ... * R_{j-1} * l_{j-1}
 
-    notice that:
+    Notice that:
         1. everything before R_i has nothing with R_i, which means it could be ignored 
            during computing the partial derivative
         2. product_{k=0,...,i-1}{R_k} = Q_{i-1}, i.e. the global rotation of joint i's parent
@@ -55,8 +55,15 @@ Consider the target j as x_j, the function item of F(θ)_j, and the correspondin
            = lim_{δθ_i -> 0} { Q_{i-1} * ( δθ_i^ * R_i * rho_{i,j} / δθ_i ) }
            = lim_{δθ_i -> 0} { - Q_{i-1} * (R_i * rho_{i,j})^ * δθ_i / δθ_i }
            = - Q_{i-1} * (R_i * rho_{i,j})^
+    the above is the computaton of limit via `left perturbation`, also it can be done via `right perturbation`:
+    ∂F_j/∂θ_i
+           = lim_{δθ_i -> 0} { ( Q_{i-1} * R_i * exp(δθ_i^) * rho_{i,j} - Q_{i-1} * R_i * rho_{i,j} ) / δθ_i }
+           = lim_{δθ_i -> 0} { ( Q_{i-1} * R_i * (I + δθ_i^) * rho_{i,j} - Q_{i-1} * R_i * rho_{i,j} ) / δθ_i }
+           = lim_{δθ_i -> 0} { Q_{i-1} * ( R_i * δθ_i^ * rho_{i,j} / δθ_i ) }
+           = lim_{δθ_i -> 0} { - Q_{i-1} * R_i * rho_{i,j}^ * δθ_i / δθ_i }
+           = - Q_{i} * rho_{i,j}^
 
-    notice that:
+    Notice that:
         1. rho_{i,j} is the local position of joint j in joint i
             since:
                 p_j = p_i + Q_i * l_i + Q_i * R_{i+1} * l_{i+1} + ... + Q_i * R_{i+1} * ... * R_{j-1} * l_{j-1}
@@ -76,12 +83,15 @@ Input:
     global_positions: (m+1, 3) ndarray
     global_rotations: (m, 4)   ndarray
     target_points:    (3,)     ndarray
+Output:
+    jacobian_matrix:  (3, 3*m) ndarray
 
 """
 
 import numpy as np
 
-from quaternion import hamilton_product, conjugate, to_axis_angle, rotate
+from quaternion import hamilton_product, conjugate, quat_normalize
+from quaternion import quat_as_matrix, quat_from_rotvec, rotate
 from scipy.spatial.transform import Rotation as R
 
 def skew_matrix(v):
@@ -92,7 +102,7 @@ def skew_matrix(v):
         [z, 0, -x],
         [-y, x, 0]])
 
-def jacobian(gp, gr, t):
+def jacobian(gp, gr, t, use_left_perturbation=False):
     """ Single target jacobian """
     assert gp.shape[-1] == 3
     assert gr.shape[-1] == 4
@@ -101,41 +111,24 @@ def jacobian(gp, gr, t):
     m = gr.shape[0]
     J = np.empty((t.shape[0], 3 * m))
     for i in range(m):
-        Q_i_1 = gr[i - 1] if i > 0 else np.array([0, 0, 0, 1]) # (seq=xyzw)identity
         Q_i = gr[i]
-        R_i = hamilton_product(conjugate(Q_i_1), Q_i)
         rho_i_j = rotate(conjugate(Q_i), gp[-1] - gp[i])
-        # J[:, i] = R.from_matrix( R.from_quat(Q_i_1).as_matrix() @ skew_matrix(rotate(R_i, rho_i_j)) ).as_rotvec()
-        J[:, 3 * i: 3 * (i + 1)] = R.from_quat(Q_i_1).as_matrix() @ skew_matrix(rotate(R_i, rho_i_j))
+        if use_left_perturbation:
+            Q_i_1 = gr[i - 1] if i > 0 else np.array([0, 0, 0, 1]) # (seq=xyzw)identity
+            R_i = quat_normalize(hamilton_product(conjugate(Q_i_1), Q_i))
+            J[:, 3 * i: 3 * (i + 1)] = -quat_as_matrix(Q_i_1) @ skew_matrix(rotate(R_i, rho_i_j))
+        else:
+            J[:, 3 * i: 3 * (i + 1)] = -quat_as_matrix(Q_i) @ skew_matrix(rho_i_j)
     return J
 
-def jacobian_cross(gp, gr, t):
-    """ Single target jacobian, by axis cross """
-    # DEBUG:
-    ### i'm so confused of its dimensions
-    ### still something abnormal ...
-    assert gp.shape[-1] == 3
-    assert gr.shape[-1] == 4
-    assert t.shape == (3,)
-    assert gp.shape[0] == gr.shape[0] + 1
-    m = gr.shape[0]
-    J = np.empty((t.shape[0], m))
-    for i in range(m):
-        R_i = gr[i] if i == 0 else hamilton_product(conjugate(gr[i - 1]), gr[i])
-        v_i, _ = to_axis_angle(R_i)
-        # aa_i = R.from_quat(R_i).as_rotvec()
-        # v_i = aa_i / (np.linalg.norm(aa_i) + 1E-8)
-        J[:, i] = np.cross(v_i, gp[-1] - gp[i])
-    return J
-
-
-def jacobian_transpose_ik(gp, gr, offset, t):
+def jacobian_transpose_ik(gp, gr, offset, t, use_left_perturbation=False, n_iter=20):
     """ delta_θ = alpha * J_T * e """
-    n_iter = 2000
     error = np.linalg.norm(t - gp[-1])
+    error_history = [error,]
     print(f"[Jacobian Transpose IK] before: error = {error}")
+
     for i in range(n_iter):
-        J = jacobian(gp, gr, t)
+        J = jacobian(gp, gr, t, use_left_perturbation)
         e = t - gp[-1]
         j_jt_e = J @ J.T @ e
         alpha = e.dot(j_jt_e) / (j_jt_e.dot(j_jt_e) + 1E-8)
@@ -144,27 +137,37 @@ def jacobian_transpose_ik(gp, gr, offset, t):
         # print(delta.shape)
         # print(delta)
 
-        lr = [q if j == 0 else hamilton_product(conjugate(gr[j - 1]), q) for j, q in enumerate(gr)]
+        lr = [q if j == 0 else quat_normalize(hamilton_product(conjugate(gr[j - 1]), q)) for j, q in enumerate(gr)]
         for j, q in enumerate(lr):
-            # # DEBUG: test if q not modified
+            # Branch-1: test if q not modified
             # lr[j] = q
 
-            # DEBUG; how to update local rotations?
-            ###      use rotvec directly adding, or use rotate
-            # lr[j] = R.from_rotvec(R.from_quat(q).as_rotvec() - delta[3 * j: 3 * (j + 1)]).as_quat()
-            lr[j] = ( R.from_quat(q) * R.from_rotvec(delta[3 * j: 3 * (j + 1)]) ).as_quat()
+            # Branch-2: how to update local rotations?
+            ###         test with both scipy~Rotation and handcrafted quaternion algebra
+            ###         use rotvec directly adding, or use rotate
+            if use_left_perturbation:
+                # lr[j] = R.from_rotvec(R.from_quat(q).as_rotvec() + delta[3 * j: 3 * (j + 1)]).as_quat()
+                # lr[j] = ( R.from_rotvec(delta[3 * j: 3 * (j + 1)]) * R.from_quat(q) ).as_quat()
+                lr[j] = quat_normalize(hamilton_product(quat_from_rotvec(delta[3 * j: 3 * (j + 1)]), q))
+            else:
+                # lr[j] = R.from_rotvec(R.from_quat(q).as_rotvec() + delta[3 * j: 3 * (j + 1)]).as_quat()
+                # lr[j] = ( R.from_quat(q) * R.from_rotvec(delta[3 * j: 3 * (j + 1)]) ).as_quat()
+                lr[j] = quat_normalize(hamilton_product(q, quat_from_rotvec(delta[3 * j: 3 * (j + 1)])))
 
-            gr[j] = lr[j] if j == 0 else hamilton_product(gr[j - 1], lr[j])
+            gr[j] = lr[j] if j == 0 else quat_normalize(hamilton_product(gr[j - 1], lr[j]))
             gp[j + 1] = gp[j] + rotate(gr[j], offset[j])
 
         error = np.linalg.norm(t - gp[-1])
+        error_history.append(error)
         print(f"[Iter:{i:02d}] in progress: alpha = {alpha:.08f}, error = {error:.08f}")
 
+    return np.array(error_history)
 
 if __name__ == '__main__':
 
     # np.random.seed(0xac1997)
 
+    # test data: generate 100 bones with random rotations and bone lengths
     n = 100
     lRs = np.random.rand(n, 4) + 1E-4 # local rotations
     lRs /= np.linalg.norm(lRs, axis=-1, keepdims=True) # unit quaternion
@@ -184,13 +187,19 @@ if __name__ == '__main__':
     target = gPs[-1] + 0.5 # add more in R^3
 
     J1 = jacobian(gp=gPs, gr=gRs, t=target)
-    J2 = jacobian_cross(gp=gPs, gr=gRs, t=target)
     print(J1.shape)
-    print(J2.shape)
     # print(J1)
-    # print(J2)
 
-    # method-1
-    jacobian_transpose_ik(gp=gPs, gr=gRs, offset=bone, t=target)
-
+    # method-1: jacobian transpose
+    err_L = jacobian_transpose_ik(
+        gp=gPs.copy(), gr=gRs.copy(), offset=bone,
+        t=target,
+        use_left_perturbation=True,
+        n_iter=50)
+    err_R = jacobian_transpose_ik(
+        gp=gPs.copy(), gr=gRs.copy(), offset=bone,
+        t=target,
+        use_left_perturbation=False,
+        n_iter=50)
+    print("if same?:=", np.abs(err_L - err_R).mean())
 
--- lab1/quaternion.py
+++ lab1/quaternion.py
@@ -1,6 +1,11 @@
 """ Quaternion Algebra """
 import numpy as np
 
+QUAT_EPSILON = 1E-16
+
+def quat_normalize(q):
+    return q / (np.linalg.norm(q) + QUAT_EPSILON)
+
 def hamilton_product(q1, q2, seq='xyzw'):
     """ quaternion product """
     assert seq in ('wxyz', 'xyzw')
@@ -27,7 +32,42 @@ def conjugate(q, seq='xyzw'):
     else:
         return np.array([-q[0], -q[1], -q[2], q[3]])
 
-def to_axis_angle(q, seq='xyzw'):
+def quat_as_matrix(q, seq='xyzw'):
+    """ quaternion to rotation_matrix """
+    assert seq in ('wxyz', 'xyzw')
+    if seq == 'wxyz':
+        w, x, y, z = q
+    else:
+        x, y, z, w = q
+    return np.array([
+        [1 - 2 * y * y - 2 * z * z,  2 * x * y - 2 * z * w,      2 * x * z + 2 * y * w],
+        [2 * x * y + 2 * z * w,      1 - 2 * x * x - 2 * z * z,  2 * y * z - 2 * x * w],
+        [2 * x * z - 2 * y * w,      2 * y * z + 2 * x * w,      1 - 2 * x * x - 2 * y * y],
+    ])
+
+def quat_from_rotvec(v, seq='xyzw'):
+    """ rotation_vector to quaternion """
+    assert seq in ('wxyz', 'xyzw')
+    x, y, z = v
+    theta = np.sqrt(x * x + y * y + z * z)
+    if theta > QUAT_EPSILON:
+        w = np.cos(0.5 * theta)
+        sin_half = np.sin(0.5 * theta)
+        scale = sin_half / theta
+        x *= scale
+        y *= scale
+        z *= scale
+    else:
+        w = 1.0
+        x = 0.0
+        y = 0.0
+        z = 0.0
+    if seq == 'wxyz':
+        return np.array([w, x, y, z])
+    else:
+        return np.array([x, y, z, w])
+
+def quat_as_axis_angle(q, seq='xyzw'):
     """ quaternion to axis_angle """
     assert seq in ('wxyz', 'xyzw')
     if seq == 'wxyz':
@@ -36,7 +76,7 @@ def to_axis_angle(q, seq='xyzw'):
         x, y, z, w = q
     theta = 2.0 * np.arccos(w)
     sin_half = np.sin(theta * 0.5)
-    if sin_half < 1E-8:
+    if sin_half < QUAT_EPSILON:
         return np.array([1, 0, 0]), 0.0
     rx = x / sin_half
     ry = y / sin_half
